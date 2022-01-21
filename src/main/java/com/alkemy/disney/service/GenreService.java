697e5158a13c2a1500f85851999799275b2a4fdd
Erika Farias <erika.aldana.farias@gmail.com>
2022-01-21 13:39:18 -0300

    Cambio de idioma, clases añadidas

---
+++ src/main/java/com/alkemy/disney/service/GenreService.java
@@ -0,0 +1,13 @@
+package com.alkemy.disney.service;
+
+import com.alkemy.disney.dto.GenreDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GenreService {
+
+    public GenreDTO save(GenreDTO dto) {
+
+        return dto;
+    }
+}
